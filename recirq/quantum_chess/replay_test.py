2bda35a35aa3c5c039b4b5d18a815692bcb5dc83
Jack Weinstein <jweinstein@google.com>
2021-05-10 09:53:53 -0700

    add test for replaying the smallest subset of recorded games

--- recirq/quantum_chess/replay_test.py
+++ recirq/quantum_chess/replay_test.py
@@ -11,6 +11,21 @@ import recirq.quantum_chess.quantum_board as qb
 import recirq.quantum_chess.testdata_utils as testdata
 
 
+ALL_CIRQ_BOARDS = [
+    qb.CirqBoard(0, error_mitigation=enums.ErrorMitigation.Error),
+    qb.CirqBoard(0,
+                 device=utils.get_device_obj_by_name('Syc54-noiseless'),
+                 error_mitigation=enums.ErrorMitigation.Error),
+    qb.CirqBoard(0,
+                 device=utils.get_device_obj_by_name('Syc23-noiseless'),
+                 error_mitigation=enums.ErrorMitigation.Error),
+    qb.CirqBoard(0,
+                 sampler=utils.get_sampler_by_name('Syc23-simulator-tester'),
+                 device=utils.get_device_obj_by_name('Syc23-simulator-tester'),
+                 error_mitigation=enums.ErrorMitigation.Correct,
+                 noise_mitigation=0.10),
+]
+
 @pytest.mark.parametrize('moves',
                          testdata.ALL_GAMES.values(),
                          ids=testdata.ALL_GAMES.keys())
@@ -18,3 +33,14 @@ def test_move_parsing_roundtrip(moves):
     for m in moves:
         move_str = m.to_string(include_type=True)
         assert move.Move.from_string(move_str) == m
+
+@pytest.mark.parametrize('board', ALL_CIRQ_BOARDS)
+@pytest.mark.parametrize('moves',
+                         testdata.TINY_GAMES.values(),
+                         ids=testdata.TINY_GAMES.keys())
+def test_tiny_replay(board, moves):
+    b = board.with_state(testdata.STANDARD_SETUP_BITBOARD)
+    for move in moves:
+        print(f'move: {move.to_string(include_type=True)}')
+        b.do_move(move)
+        b.sample(100)

7d412ef37d9c6b9355bbaf7c74186496270cf60d
Jack Weinstein <jweinstein@google.com>
2021-06-08 11:27:37 -0700

    update test naming to include the board name

--- recirq/quantum_chess/replay_test.py
+++ recirq/quantum_chess/replay_test.py
@@ -1,6 +1,7 @@
 import pytest
 import cirq
 import os
+import sys
 from typing import List
 
 import recirq.engine_utils as utils
@@ -11,20 +12,24 @@ import recirq.quantum_chess.quantum_board as qb
 import recirq.quantum_chess.testdata_utils as testdata
 
 
-ALL_CIRQ_BOARDS = [
-    qb.CirqBoard(0, error_mitigation=enums.ErrorMitigation.Error),
-    qb.CirqBoard(0,
+CIRQ_BOARDS = {
+    'default': qb.CirqBoard(0, error_mitigation=enums.ErrorMitigation.Error),
+    'Syc54-noiseless': qb.CirqBoard(0,
                  device=utils.get_device_obj_by_name('Syc54-noiseless'),
                  error_mitigation=enums.ErrorMitigation.Error),
-    qb.CirqBoard(0,
+    'Syc23-noiseless': qb.CirqBoard(0,
                  device=utils.get_device_obj_by_name('Syc23-noiseless'),
                  error_mitigation=enums.ErrorMitigation.Error),
-    qb.CirqBoard(0,
-                 sampler=utils.get_sampler_by_name('Syc23-simulator-tester'),
-                 device=utils.get_device_obj_by_name('Syc23-simulator-tester'),
+    'Syc23-simulator': qb.CirqBoard(0,
+                 sampler=utils.get_sampler_by_name('Syc23-simulator'),
+                 device=utils.get_device_obj_by_name('Syc23-simulator'),
                  error_mitigation=enums.ErrorMitigation.Correct,
                  noise_mitigation=0.10),
-]
+    # 'weber': qb.CirqBoard(0, sampler=utils.get_sampler_by_name('Sycamore54', gateset='sqrt-iswap'),
+    #                device=utils.get_device_obj_by_name('Sycamore54'),
+    #                error_mitigation=enums.ErrorMitigation.Correct,
+    #                noise_mitigation=0.10),
+}
 
 @pytest.mark.parametrize('moves',
                          testdata.ALL_GAMES.values(),
@@ -34,13 +39,15 @@ def test_move_parsing_roundtrip(moves):
         move_str = m.to_string(include_type=True)
         assert move.Move.from_string(move_str) == m
 
-@pytest.mark.parametrize('board', ALL_CIRQ_BOARDS)
+@pytest.mark.parametrize('board', CIRQ_BOARDS.values(), ids=CIRQ_BOARDS.keys())
 @pytest.mark.parametrize('moves',
-                         testdata.TINY_GAMES.values(),
-                         ids=testdata.TINY_GAMES.keys())
-def test_tiny_replay(board, moves):
+                         testdata.ALL_GAMES.values(),
+                         ids=testdata.ALL_GAMES.keys())
+def test_game_replay(board, moves, request):
+    print(f'running {request.node.callspec.id}')
     b = board.with_state(testdata.STANDARD_SETUP_BITBOARD)
     for move in moves:
         print(f'move: {move.to_string(include_type=True)}')
         b.do_move(move)
         b.sample(100)
+
